bd92fd73b4b870506d2626f54a17bc1446926206
DevHyperCoder <devan@devhypercoder.com>
2022-01-30 08:26:42 +0530

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'anonpoll-dhc',
+    appId: '1:233234762302:web:4089208775ac4308badf6f',
+    storageBucket: 'anonpoll-dhc.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBpYB4Zo8erEvhPYTpIm6vzgX3mmpdhgVM',
+    authDomain: 'anonpoll-dhc.firebaseapp.com',
+    messagingSenderId: '233234762302',
+  },
   production: false
 };
 
